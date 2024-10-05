9ffbfd8d9f7bc0265a90874b3c4b218d9b767077
Pimping2k <125730140+Pimping2k@users.noreply.github.com>
2024-10-06 00:35:55 +0300

    Andrei branch     (#7)
    * Make a logic for spawn people
    Added first level
    
    * Made change game state logic
    Made container to save each constant variables
    
    * Added Camera controll when you drag sinner
    
    * Andrei branch (#2)
    
    * Make a logic for spawn people
    Added first level
    
    * Made change game state logic
    Made container to save each constant variables
    
    * new files
    
    * Add the throne and the chains (#3)
    
    * Make a logic for spawn people
    Added first level
    
    * Made change game state logic
    Made container to save each constant variables
    
    * Added Camera controll when you drag sinner
    
    * Andrei branch (#2)
    
    * Make a logic for spawn people
    Added first level
    
    * Made change game state logic
    Made container to save each constant variables
    
    * Added Camera controll when you drag sinner
    
    * Save changes
    
    * Save
    
    * Save
    
    * Added event for show/hide sinner information
    
    * Save changes
    
    ---------
    
    Co-authored-by: kokostyrko <alexandracocostirco@gmail.com>
    Co-authored-by: kokostyrko <146729675+kokostyrko@users.noreply.github.com>

--- Assets/Scripts/GameScene/SinnerCharacteristcsComponent.cs
+++ Assets/Scripts/GameScene/SinnerCharacteristcsComponent.cs
@@ -1,18 +1,58 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SinnerCharacteristcsComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<string> sins = new List<string>()
     {
-        
-    }
+        "Wasteful",
+        "Profligate",
+        "Slothful",
+        "False Teacher",
+        "Blasphemer",
+        "Suicide",
+        "Lustful",
+        "Glutton",
+        "Greedy",
+        "Wrathful",
+        "Heretic",
+        "Tyrant",
+        "Deceiver",
+        "Betrayer"
+    };
 
-    // Update is called once per frame
-    void Update()
+    private List<string> names = new List<string>()
     {
-        
+        "Alexander",
+        "Dmitry",
+        "Irene",
+        "Anna",
+        "Sergey",
+        "Catherine",
+        "Nicholas",
+        "Olga",
+        "Maxim",
+        "Tatiana",
+        "Andrew",
+        "Natalia",
+        "Eugene",
+        "Maria",
+        "Victor",
+        "Svetlana",
+        "Artem",
+        "Ksenia",
+        "Vasily",
+        "Julia"
+    };
+
+    public string name;
+    public string sin;
+
+    private void Awake()
+    {
+        name = names[Random.Range(0, names.Count)];
+        sin = sins[Random.Range(0, sins.Count)];
     }
 }
