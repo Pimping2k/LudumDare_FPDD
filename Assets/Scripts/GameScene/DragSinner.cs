45fbc72aa1d0d834ad6092745efcd7110dd95816
Pimping . <auagfuiawg@mail.ru>
2024-10-06 00:26:26 +0300

    Added event for show/hide sinner information

--- Assets/Scripts/GameScene/DragSinner.cs
+++ Assets/Scripts/GameScene/DragSinner.cs
@@ -1,13 +1,19 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DragSinner : MonoBehaviour
 {
+    [SerializeField] private Text sinnerInfo;
+    [SerializeField] private Image sinnerInfoBg;
     private Vector3 offset;
     private float mZCoord;
+    
     private void OnMouseDown()
     {
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        
+        UpdateSinnerInformation();
+        ShowInfo();
         offset = gameObject.transform.position - GetMouseWorldPosition();
     }
 
@@ -16,11 +22,36 @@ public class DragSinner : MonoBehaviour
         gameObject.transform.position = GetMouseWorldPosition() + offset;
     }
 
+    private void OnMouseUp()
+    {
+        HideInfo();
+    }
+
     Vector3 GetMouseWorldPosition()
     {
         Vector3 mousePoint = Input.mousePosition;
         mousePoint.z = mZCoord;
-        
+
         return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void ShowInfo()
+    {
+        sinnerInfo.gameObject.SetActive(true);
+        sinnerInfoBg.gameObject.SetActive(true);
+    }
+    
+    void HideInfo()
+    {
+        sinnerInfo.gameObject.SetActive(false);
+        sinnerInfoBg.gameObject.SetActive(false);
+    }
+
+    void UpdateSinnerInformation()
+    {
+        var sinnerInfoComponent = this.GetComponent<SinnerCharacteristcsComponent>();
+        sinnerInfo.text = "Sinner:\n" +
+                          $"Name - {sinnerInfoComponent.name}\n" +
+                          $"Sin - {sinnerInfoComponent.sin}";
     }
 }
---
+++ Assets/Scripts/GameScene/SetSinnerInformation.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetSinnerInformation : MonoBehaviour
+{
+    [SerializeField] private GameObject sinner;
+
+    private void Start()
+    {
+        var sinnerCharacteristic = sinner.GetComponent<SinnerCharacteristcsComponent>();
+        var sinnerText = this.GetComponent<Text>();
+
+        sinnerText.text = "Sinner:\n" +
+                          $"Name - {sinnerCharacteristic.name}\n" +
+                          $"Sin - {sinnerCharacteristic.sin}";
+    }
+}
