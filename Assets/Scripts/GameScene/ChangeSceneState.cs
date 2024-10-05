dbf943e31d51e300c07aadbac36b8425bd0c5dba
Pimping . <auagfuiawg@mail.ru>
2024-10-05 21:22:44 +0300

    Added Camera controll when you drag sinner

--- Assets/Scripts/GameScene/ChangeSceneState.cs
+++ Assets/Scripts/GameScene/ChangeSceneState.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeSceneState : MonoBehaviour
 {
     private Camera _camera;
-    [SerializeField] float duration = 10f;
+    [SerializeField] float duration = 0.5f;
+    [SerializeField] private Button returnButton;
 
     private void Start()
     {
@@ -16,24 +18,23 @@ public class ChangeSceneState : MonoBehaviour
         if (other.CompareTag(Container.SINNER))
         {
             StartCoroutine(RotateCamera());
+            returnButton.gameObject.SetActive(true);
         }
     }
 
     IEnumerator RotateCamera()
     {
         Quaternion targetRotation = Quaternion.Euler(0, 90, 0);
+        Quaternion initialRotation = _camera.transform.rotation;
         float elapsedTime = 0f;
 
         while (elapsedTime < duration)
         {
-            _camera.transform.rotation =
-                Quaternion.Slerp(_camera.transform.rotation, targetRotation, (elapsedTime / duration));
+            _camera.transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime / duration);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         _camera.transform.rotation = targetRotation;
-
-        yield return new WaitForSeconds(1f);
     }
 }
