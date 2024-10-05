8882a0512d76900dfe15310c4dd7c26f8ce4d7d2
Pimping . <auagfuiawg@mail.ru>
2024-10-05 21:22:44 +0300

    Added Camera controll when you drag sinner

--- Assets/Scripts/Container.cs
+++
@@ -1,6 +0,0 @@
-public static class Container
-{
-    public const string SINNER = "Sinner";
-    public const string GAMESCENE = "GameScene";
-    public const string CHOOSE_TRICK_SCENE = "ChooseTrickScene";
-}
